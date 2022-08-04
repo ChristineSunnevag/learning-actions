6bd51d5c95542ce145d102a17e75b8ee1195702d
ChristineSunnevag <107137482+ChristineSunnevag@users.noreply.github.com>
2022-08-04 08:55:07 +0200

    Added a new unit test

--- SampleFunctionApp.Test/FunctionsTest.cs
+++ SampleFunctionApp.Test/FunctionsTest.cs
@@ -17,7 +17,13 @@ namespace SampleFunctionApp.Test
             Assert.Equal("Hello Bill! Welcome to Azure Functions!", response.Value);
         }
         
-        
+        [Fact]
+         public async void HttpTriggerWithParams2()
+         {
+             var request = TestFactory.CreateHttpRequest("name", "Nancy");
+             var response = (OkObjectResult)await Function1.Run(request, logger);
+             Assert.Equal("Hello Nancy! Welcome to Azure Functions!", response.Value);
+         }
 
         [Fact]
         public async void HttpTriggerWithoutParams()
@@ -26,5 +32,6 @@ namespace SampleFunctionApp.Test
             var response = (OkObjectResult)await Function1.Run(request, logger);
             Assert.Equal("Hello there! Welcome to Azure Functions!", response.Value);
         }
+        
     }
 }
